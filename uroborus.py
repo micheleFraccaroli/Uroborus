e7dd20be0ccdf3955fc3c5c86fbbb1791db7d140
micheleFraccaroli <michelefraccaroli@gmail.com>
2017-06-06 13:09:16 +0200

    Draggable_legend

--- uroborus.py
+++ uroborus.py
@@ -25,7 +25,6 @@ import fileinput
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 
 # function-----------------------------------
@@ -252,6 +251,7 @@ while 1:
         data2 = input('Inserisci il secondo file: ')
         data1_path = ("Input_file/s3/" + data1)
         data2_path = ("Input_file/s3/" + data2)
+        legend = input("Inserisci legenda: ")
         try:
             if data1 != 'q' or data2 != 'q':
                 while(data1 != 'ok' or data2 != 'ok'):
@@ -261,13 +261,19 @@ while 1:
                     for i in range(len(column_y_1)):
                         column_ris.append(float(column_y_1[i]) - float(column_y_2[i]))
 
-                    trace_ris.append(plt.plot(column_x_1, column_ris))
+                    trace_ris.append(plt.plot(column_x_1, column_ris, label=legend))
                     column_ris.clear()
                     data1 = input("Inserisci il primo file('ok' per eseguire): ")
                     data2 = input("Inserisci il secondo file('ok' per eseguire): ")
                     data1_path = ("Input_file/s3/" + data1)
                     data2_path = ("Input_file/s3/" + data2)
+                    if data1 == 'ok' or data2 == 'ok':
+                        break
+                    else:
+                        legend = input("Inserisci legenda: ")
 
+                let = plt.legend()
+                let.draggable()
                 title = input("Inserisci il titolo del grafico: ")
                 xname = input("Inserisci il nome dell'asse x: ")
                 yname = input("Inserisci il nome dell'asse y: ")
@@ -309,7 +315,8 @@ while 1:
                 xname = input("Inserisci il nome dell'asse x: ")
                 yname = input("Inserisci il nome dell'asse y: ")
 
-                plt.legend()
+                let = plt.legend()
+                let.draggable()
                 plt.suptitle(title, fontsize=12, fontweight='bold')
                 plt.xlabel(xname)
                 plt.ylabel(yname)
